c83ac13b8d7154ab3a167769fa0c556730245f94
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-03-02 12:03:25 -0500

    Itegrated new & create action for portfolios

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,21 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params.require(:project).permit(:title,:subtitle,:body))
+
+    respond_to do |format|
+      if @project.save
+        format.html {redirect_to projects_path, notice:"Your project has been added"}
+      else
+        format.html { render:new }
+      end
+    end
+  end
+
 end
