784dc73f3a69734a64920589e8ed5dba142a6166
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-05-18 12:50:46 -0400

    Customized routes for devise registration and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :projects, except: [:show]
   get 'projects/:id', to:'projects#show', as:"project_show"
 
