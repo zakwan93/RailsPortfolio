aee7056bfeb631d788ad53ba3ffb724003c3cca9
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-06-01 20:09:08 -0400

    Installed jquery ui, sortable html5 library, and css calls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,12 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+// import {} from 'jquery-ujs'
 import 'bootstrap/dist/js/bootstrap'
+require("jquery_ujs")
 import 'bootstrap/dist/css/bootstrap'
 require("stylesheets/application.scss")
+require("packs/html.sortable")
 import "@fortawesome/fontawesome-free/css/all"
 // require("trix")
 // require("@rails/actiontext")
