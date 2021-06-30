d18820b3a5a154051b44f2ccff39e8dd62d2da34
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-06-30 14:19:24 -0400

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,7 @@ class Project < ApplicationRecord
     accepts_nested_attributes_for :technologies, 
                                   reject_if: lambda { |attrs| attrs['name'].blank?  } 
 
-    include Placeholder
-    validates_presence_of :title, :body, :main_image, :thumb_image
+    validates_presence_of :title, :body
 
     mount_uploader :thumb_image, ProjectUploader
     mount_uploader :main_image, ProjectUploader
@@ -19,14 +18,6 @@ class Project < ApplicationRecord
 
     scope :ruby_on_rails_project_items, -> {where(subtitle: "Ruby on Rails")}
 
-    after_initialize :set_defaults
-
-    def set_defaults
-      # self.main_image ||= "https://via.placeholder.com/600x400"
-      # self.thumb_image ||= "https://via.placeholder.com/350x200"
-      self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-      self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
 
     extend FriendlyId
     friendly_id :title, use: :slugged
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,12 +1,12 @@
 class Skill < ApplicationRecord
-    include Placeholder
+    # include Placeholder
 
     validates_presence_of :title, :proficiency
 
-    after_initialize :set_defaults
+    # after_initialize :set_defaults
 
-    def set_defaults
-      # self.badge ||= "https://via.placeholder.com/250x250"
-      self.badge ||= Placeholder.image_generator(height: '250', width: '250')
-    end
+    # def set_defaults
+    #   # self.badge ||= "https://via.placeholder.com/250x250"
+    #   self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+    # end
 end
